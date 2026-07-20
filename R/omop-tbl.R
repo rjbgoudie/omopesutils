4288c70cc8c84d07bb14f2b3f4bf5beb0a7727e4
rjbgoudie <rjbgoudie@users.noreply.github.com>
2026-07-20 14:57:53 +0100

    Fixup `tbl_*` and `id_*` functions

--- R/omop-tbl.R
+++ R/omop-tbl.R
@@ -1,21 +1,37 @@
+#' @importFrom DBI Id
 id_omop <- function(table_name, schema = "dbo") {
   DBI::Id(table = table_name, schema = schema)
 }
 
+#' @importFrom DBI Id
+#' @importFrom glue glue
 id_omop_links <- function(table_name, schema = "priv") {
   DBI::Id(schema = schema, table = glue::glue("{table_name}_links"))
 }
 
 #' @importFrom DBI Id
+#' @importFrom dplyr tbl
 tbl_omop <- function(conn, table_name, schema = "dbo") {
-  tbl(conn, id_omop(table_name, schema))
+  dplyr::tbl(conn, id_omop(table_name, schema))
 }
 
 #' @importFrom DBI Id
+#' @importFrom dplyr tbl
 tbl_omop_links <- function(conn, table_name, schema = "priv") {
-  tbl(conn, id_omop_links(table_name, schema))
+  dplyr::tbl(conn, id_omop_links(table_name, schema))
 }
 
-all_omop_tables <- function(conn, schema = "dbo") {
-  dbListTablesAndViewsInSchema(conn, schema)
+#' @export
+dbListOmopTables <- function(conn, schema = "dbo", exclude_vocab = TRUE) {
+  result <- dbListTablesAndViewsInSchema(conn, schema)
+  if (exclude_vocab){
+    result <- setdiff(result, omop_vocab_tables())
+  }
+  result
+}
+
+#' @importFrom DBI Id
+#' @importFrom dplyr tbl
+tbl_omop_concept <- function(conn) {
+  dplyr::tbl(conn, id_omop(table_name = "concept", schema = "dbo"))
 }
