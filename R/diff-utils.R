98ca3c9b56c663d6d73b50b675d1dd90c2da1970
rjbgoudie <rjbgoudie@users.noreply.github.com>
2026-07-22 17:00:52 +0100

    Fixup `omop_es_tables_in_either_db()`
    Use the new `dbListOmopTables` function, and add `exclude_vocab` option

--- R/diff-utils.R
+++ R/diff-utils.R
@@ -1,9 +1,18 @@
 omop_es_tables_in_either_db <- function(
   conn,
   schema_public1 = "dbo",
-  schema_public2 = "dbo2"
+  schema_public2 = "dbo2",
+  exclude_vocab = TRUE
 ) {
-  table_names1 <- all_omop_tables(conn, schema_public1)
-  table_names2 <- all_omop_tables(conn, schema_public2)
+  table_names1 <- dbListOmopTables(
+    conn,
+    schema_public1,
+    exclude_vocab = exclude_vocab
+  )
+  table_names2 <- dbListOmopTables(
+    conn,
+    schema_public2,
+    exclude_vocab = exclude_vocab
+  )
   union(table_names1, table_names2)
 }
